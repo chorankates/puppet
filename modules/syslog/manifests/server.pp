5b5605a225c1cd26770328b9e10b96316e6819ff
Conor Horan-Kates <conor.code@gmail.com>
2016-07-29 13:43:49 -0700

    TODO comments for syslog changes to make

--- modules/syslog/manifests/server.pp
+++ modules/syslog/manifests/server.pp
@@ -5,4 +5,14 @@ class syslog::server {
   # TODO use augeas to modify
   notify { 'SYSLOG CLIENT NOT IMPLEMENTED': }
 
+  # /etc/rsyslog.conf needs to have these:
+  # provides UDP syslog reception
+  #module(load="imudp")
+  #input(type="imudp" port="514")
+
+  # provides TCP syslog reception
+  #module(load="imtcp")
+  #input(type="imtcp" port="514")
+
+
 }
