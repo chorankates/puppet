6a54d590ee001b780d5f8e205ab2af7032308994
Conor Horan-Kates <conor.code@gmail.com>
2016-07-29 13:41:53 -0700

    clean up hieradata path, add squid

--- modules/squid/manifests/init.pp
+++ modules/squid/manifests/init.pp
@@ -2,6 +2,18 @@
 
 class squid {
 
-  notify { 'SQUID NOT IMPLEMENTED': }
+  notify { 'SQUID NOT CONFIGURED': }
+
+  package { 'squid':
+    ensure => latest,
+  }
+
+  service { 'squid':
+    ensure  => 'running',
+    enabled => 1,
+  }
+
+  # TODO configure, need to pull port from heira
+
 
 }
